2b3ba1fbebf3bc79704da3e04c02c98622eeb68d
Daniel Lesko <dles@ciklum.com>
2023-08-10 10:38:37 +0200

    detail for job page is working

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,10 +1,13 @@
-import { getJobs } from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+import { getJob, getJobs } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
+    job: (_root, { id }) => getJob(id),
     jobs: () => getJobs(),
   },
   Job: {
+    company: (job) => getCompany(job.companyId),
     date: (job) => toIsoDate(job.createdAt),
   },
 };
