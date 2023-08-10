df792a96475a6a3eb8f4ae4215842d8db686bcaf
Daniel Lesko <dles@ciklum.com>
2023-08-10 15:23:28 +0200

    post operation is done and working

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,49 +2,24 @@ import { GraphQLClient, gql } from "graphql-request";
 
 const client = new GraphQLClient("http://localhost:9000/graphql");
 
-export async function getJobs() {
-  const query = gql`
-    query {
-      jobs {
-        title
-        date
+export async function createJob({ title, description }) {
+  const mutation = gql`
+    mutation CreateJob($input: CreateJobInput!) {
+      job: createJob(input: $input) {
         id
-        company {
-          id
-          name
-        }
       }
     }
   `;
-  const { jobs } = await client.request(query);
-  return jobs;
-}
-
-export async function getJob(id) {
-  const query = gql`
-    query JobById($id: ID!) {
-      job(id: $id) {
-        id
-        date
-        title
-        description
-        company {
-          id
-          name
-          description
-        }
-      }
-    }
-  `;
-
-  const { job } = await client.request(query, { id });
+  const { job } = await client.request(mutation, {
+    input: { title, description },
+  });
   return job;
 }
 
-export async function getCompany(companyId) {
+export async function getCompany(id) {
   const query = gql`
-    query CompanyById($companyId: ID!) {
-      company(id: $companyId) {
+    query CompanyById($id: ID!) {
+      company(id: $id) {
         id
         name
         description
@@ -56,6 +31,43 @@ export async function getCompany(companyId) {
       }
     }
   `;
-  const { company } = await client.request(query, { companyId });
+  const { company } = await client.request(query, { id });
   return company;
+}
+
+export async function getJob(id) {
+  const query = gql`
+    query JobById($id: ID!) {
+      job(id: $id) {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const { job } = await client.request(query, { id });
+  return job;
+}
+
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await client.request(query);
+  return jobs;
 }
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,9 +1,23 @@
 import { GraphQLError } from "graphql";
 import { getCompany } from "./db/companies.js";
-import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import {
+  createJob,
+  deleteJob,
+  getJob,
+  getJobs,
+  getJobsByCompany,
+  updateJob,
+} from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("No Company found with id " + id);
+      }
+      return company;
+    },
     job: async (_root, { id }) => {
       const job = await getJob(id);
       if (!job) {
@@ -12,29 +26,37 @@ export const resolvers = {
       return job;
     },
     jobs: () => getJobs(),
-    company: async (_root, { id }) => {
-      const company = await getCompany(id);
-      if (!company) {
-        throw notFoundError("No Company found with id " + id);
-      }
-      return company;
+  },
+
+  Mutation: {
+    createJob: (_root, { input: { title, description } }) => {
+      const companyId = "FjcJCHJALA4i"; // TODO set based on user
+      return createJob({ companyId, title, description });
+    },
+
+    deleteJob: (_root, { id }) => deleteJob(id),
+
+    updateJob: (_root, { input: { id, title, description } }) => {
+      return updateJob({ id, title, description });
     },
   },
+
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+
   Job: {
     company: (job) => getCompany(job.companyId),
     date: (job) => toIsoDate(job.createdAt),
   },
-  Company: {
-    jobs: (company) => getJobsByCompany(company.id),
-  },
 };
-
-function toIsoDate(value) {
-  return value.slice(0, "yyyy-mm-dd".length);
-}
 
 function notFoundError(message) {
   return new GraphQLError(message, {
     extensions: { code: "NOT_FOUND" },
   });
+}
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
 }
