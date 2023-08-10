00b3a607f0cf05a0f7b197d7d74ecd1c1bb633e5
Daniel Lesko <dles@ciklum.com>
2023-08-10 14:18:03 +0200

    error handling added

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,21 +1,40 @@
+import { GraphQLError } from "graphql";
 import { getCompany } from "./db/companies.js";
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
-    job: (_root, { id }) => getJob(id),
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("No Job found with id " + id);
+      }
+      return job;
+    },
     jobs: () => getJobs(),
-    company: (_root, { id }) => getCompany(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("No Company found with id " + id);
+      }
+      return company;
+    },
   },
   Job: {
     company: (job) => getCompany(job.companyId),
     date: (job) => toIsoDate(job.createdAt),
   },
   Company: {
-    jobs: (company) => getJobsByCompany(company.id),
+    jobs: (company) => getJobsByCompany(company.idf),
   },
 };
 
 function toIsoDate(value) {
   return value.slice(0, "yyyy-mm-dd".length);
+}
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
 }
